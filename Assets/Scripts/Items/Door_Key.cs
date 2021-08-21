2a358239de35a8e4fdbc4536dfc92373f8830180
fermingimpel <fermingimpel@gmail.com>
2021-08-21 20:44:31 -0300

    Añadida funcionalidad de pickupear keys y abrir puertas con la llave

---
+++ Assets/Scripts/Items/Door_Key.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Key : MonoBehaviour {
+    [SerializeField] bool canUseKey = true;
+    [SerializeField] bool canPickUp = true;
+    public bool GetCanPickUp() {
+        return canPickUp;
+    }
+    public bool GetCanUseKey() {
+        return canUseKey;
+    }
+    public void UseKey() {
+        Destroy(this.gameObject, 0.1f);
+    }
+    public Door_Key PickUpItem() {
+        transform.position = new Vector3(0, -999, 0);
+        canPickUp = false;
+        return this;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerPickUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickUp : MonoBehaviour {
+    [SerializeField] Inventory inventory;
+    [SerializeField] LayerMask pickUpKey;
+
+   public void TryPickUpObject() {
+        RaycastHit hit;
+        Vector3 mousePos = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        if (Physics.Raycast(ray, out hit, 3))
+            if (pickUpKey == (pickUpKey | (1 << hit.transform.gameObject.layer))) {
+                Door_Key item = hit.collider.GetComponent<Door_Key>();
+                if (item != null && item.GetCanPickUp())
+                    inventory.AddKeyToInventory(item.PickUpItem());
+            }
+    }
+}
