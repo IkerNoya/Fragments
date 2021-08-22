13574e9394ed0f7d4b2bc6900e0546717c3f15fa
IkerNoya <ikernoya4@gmail.com>
2021-08-22 00:28:32 -0300

    Added text to show Interactable objects

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,35 +1,40 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    
     [SerializeField] Inventory playerInventory;
     [SerializeField] PlayerPickUp playerPickUpController;
-
+    [Space]
     [SerializeField] KeyCode keyToPickUpItem;
     [SerializeField] KeyCode keyToInteractWithEnviroment;
     [SerializeField] LayerMask layerDoors;
 
+    public static event Action<bool> ActivateText;
+
     void Update() {
-        if (Input.GetKeyDown(keyToPickUpItem))
-            playerPickUpController.TryPickUpObject();
-        if (Input.GetKeyDown(keyToInteractWithEnviroment)) 
-            TryInteractWithDoor();
+        playerPickUpController.TryPickUpObject(keyToPickUpItem);
+        TryInteractWithDoor(keyToInteractWithEnviroment);
     }
 
-    void TryInteractWithDoor() {
+    void TryInteractWithDoor(KeyCode key) {
         RaycastHit hit;
         Vector3 mousePos = Input.mousePosition;
         Ray ray = Camera.main.ScreenPointToRay(mousePos);
 
         if (Physics.Raycast(ray, out hit, 3))
-            if (layerDoors == (layerDoors | (1 << hit.transform.gameObject.layer))) {
+            if (layerDoors == (layerDoors | (1 << hit.transform.gameObject.layer)))
+            {
+                ActivateText.Invoke(true);
                 Door_Base d = hit.transform.GetComponent<Door_Base>();
-                if (d != null) {
+                if (d != null && Input.GetKeyDown(key))
+                {
                     List<Door_Key> keyListAux = playerInventory.GetInventoryKeysList();
                     for (int i = 0; i < keyListAux.Count; i++)
-                        if (d.TryOpenDoor(keyListAux[i])) {
+                        if (d.TryOpenDoor(keyListAux[i]))
+                        {
                             d.OpenDoor();
                             keyListAux[i].UseKey();
                             keyListAux.RemoveAt(i);
@@ -37,5 +42,7 @@ public class PlayerController : MonoBehaviour {
                         }
                 }
             }
+            else
+                ActivateText.Invoke(false);
     }
 }
---
+++ Assets/Scripts/Player/PlayerHUD.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHUD : MonoBehaviour
+{
+    [SerializeField] GameObject openDoorText;
+    [SerializeField] GameObject pickUpText;
+
+    bool pickUpEvent;
+    bool doorEvent;
+
+    void Start()
+    {
+        PlayerPickUp.ActivateText += GetPickupEvent;
+        PlayerController.ActivateText += GetDoorEvent;
+    }
+
+    void Update()
+    {
+        if (pickUpEvent) pickUpText.SetActive(true);
+        else pickUpText.SetActive(false);
+
+        if (doorEvent) openDoorText.SetActive(true);
+        else openDoorText.SetActive(false);
+    }
+
+    void GetPickupEvent(bool value)
+    {
+        pickUpEvent = value;
+    }
+
+    void GetDoorEvent(bool value)
+    {
+        doorEvent = value;
+    }
+
+    private void OnDisable()
+    {
+        PlayerPickUp.ActivateText -= GetPickupEvent;
+        PlayerController.ActivateText -= GetDoorEvent;
+    }
+}
