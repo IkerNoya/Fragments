72a79bd9d96907f72654f2c6b6a03abfa33fb1e6
IkerNoya <ikernoya4@gmail.com>
2021-09-10 17:52:39 -0300

    Added dialogue on initial cutscene

--- Assets/Scripts/GameManager/InitialCutscene.cs
+++ Assets/Scripts/GameManager/InitialCutscene.cs
@@ -7,6 +7,8 @@ public class InitialCutscene : MonoBehaviour
     [SerializeField] Image blackScreen;
     [SerializeField] float screenFadeSpeed;
     [SerializeField] bool hasPlayedInitialCutscene = false;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip dialogue;
 
     public static Action<bool> initialCutscene;
     public static Action<bool> endInitialCutscene;
@@ -17,6 +19,8 @@ public class InitialCutscene : MonoBehaviour
     void Start()
     {
         anim = GetComponent<Animator>();
+
+        audioSource = GetComponentInParent<AudioSource>();
 
         if (!hasPlayedInitialCutscene)
         {
@@ -35,6 +39,11 @@ public class InitialCutscene : MonoBehaviour
             alpha -= Time.deltaTime * screenFadeSpeed;
             blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, alpha);
         }
+    }
+
+    public void PlayAudio()
+    {
+        audioSource.PlayOneShot(dialogue);
     }
 
     public void InitialCutsceneHasEnded()
