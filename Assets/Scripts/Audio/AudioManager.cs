eea475667e8d2aa59f385383da7ddffe258a3ad0
IkerNoya <ikernoya4@gmail.com>
2021-10-29 19:59:13 -0300

    Created audio manager with ambient and clock sound

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioClip Ambient;
+    [SerializeField] AudioSource CameraSource;
+    [SerializeField] AudioSource ClockSource;
+    bool isPaused = false;
+    
+    void Start()
+    {
+
+        PauseController.SetPause += Pause;
+
+        CameraSource.clip = Ambient;
+        CameraSource.Play();
+    }
+
+    public void PlayClockSound()
+    {
+        ClockSource.Play();
+    }
+
+    void Pause(bool value)
+    {
+        isPaused = value;
+        PauseSFX();
+    }
+
+    void PauseSFX()
+    {
+        if (isPaused)
+            ClockSource.Pause();
+        else if (!isPaused && !ClockSource.isPlaying)
+            ClockSource.UnPause();
+    }
+
+    private void OnDestroy()
+    {
+        PauseController.SetPause -= Pause;
+    }
+}
