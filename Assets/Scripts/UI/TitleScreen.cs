63e8cf39666573a39cf535d8b0efb056a2ef68f8
Iker <ikernoya4@gmail.com>
2021-11-10 12:13:01 -0300

    Created titlescreen and changed design of credits scene

---
+++ Assets/Scripts/UI/TitleScreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    [SerializeField] Image EnterButton;
+
+    bool canInteract = false;
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (!canInteract)
+            return;
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            anim.SetTrigger("out");
+        }
+    } 
+    public void SetCanInteract()
+    {
+        canInteract = !canInteract;
+    }
+
+    public void MoveToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
