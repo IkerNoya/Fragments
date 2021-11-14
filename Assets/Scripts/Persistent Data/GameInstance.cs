a8d84feefc8a195e0d8d5d91a09eb8cae3d209b3
Iker <ikernoya4@gmail.com>
2021-11-14 13:10:01 -0300

    Completed options menu functionality

---
+++ Assets/Scripts/Persistent Data/GameInstance.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstance : MonoBehaviour
+{
+    private float m_horizontalSensitivity = 0;
+    private float m_verticalSensitivity = 0;
+    private float m_sfxVolume = 0;
+    private float m_musicVolume = 0;
+
+
+    [HideInInspector]
+    public float horitontalSensitivity { get { return m_horizontalSensitivity; } set { m_horizontalSensitivity = value; } }
+    [HideInInspector]
+    public float verticalSensitivity { get { return m_verticalSensitivity; } set { m_verticalSensitivity = value; } }
+    [HideInInspector]
+    public float sfxVolume { get { return m_sfxVolume; } set { m_sfxVolume = value; } }
+    [HideInInspector]
+    public float musicVolume { get { return m_musicVolume; } set { m_musicVolume = value; } }
+
+
+    static GameInstance instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(this);
+    }
+
+    public static GameInstance Get()
+    {
+        return instance;
+    }
+    
+}
--- Assets/Scripts/UI/UIOptions.cs
+++ Assets/Scripts/UI/UIOptions.cs
@@ -1,18 +1,74 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIOptions : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    GameInstance gameInstance;
+
+    float horizontalSensitivity = 0;
+    float verticalSensitivity = 0;
+    float sfxVolume = 0;
+    float musicVolume = 0;
+
+    [Header("Sliders")]
+    [SerializeField] Slider h_sensitivity;
+    [SerializeField] Slider v_sensitivity;
+    [SerializeField] Slider sfx_volume;
+    [SerializeField] Slider music_volume;
+    [Header("Values")]
+    [SerializeField] Text hValue;
+    [SerializeField] Text vValue;
+    [SerializeField] Text sfxValue;
+    [SerializeField] Text musicValue;
+
+
     void Start()
     {
-        
+        gameInstance = GameInstance.Get();
+        if (gameInstance != null)
+        {
+            horizontalSensitivity = gameInstance.horitontalSensitivity;
+            verticalSensitivity = gameInstance.verticalSensitivity;
+            sfxVolume = gameInstance.sfxVolume;
+            musicVolume = gameInstance.musicVolume;
+        }
+
+        h_sensitivity.value = horizontalSensitivity;
+        v_sensitivity.value = verticalSensitivity;
+        sfx_volume.value = sfxVolume;
+        music_volume.value = musicVolume;
+
+        hValue.text = ((int)horizontalSensitivity).ToString();
+        vValue.text = ((int)verticalSensitivity).ToString();
+        sfxValue.text = ((int)sfxVolume).ToString();
+        musicValue.text = ((int)musicVolume).ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateSensitivityH()
     {
-        
+        horizontalSensitivity = h_sensitivity.value;
+        hValue.text = ((int)horizontalSensitivity).ToString();
+        gameInstance.horitontalSensitivity = horizontalSensitivity;
+    }
+    public void UpdateSensitivityV()
+    {
+        verticalSensitivity = v_sensitivity.value;
+        vValue.text = ((int)verticalSensitivity).ToString();
+        gameInstance.verticalSensitivity = verticalSensitivity;
+    }
+    public void UpdateSFXVolume()
+    {
+        sfxVolume = sfx_volume.value;
+        sfxValue.text = ((int)sfxVolume).ToString();
+        gameInstance.sfxVolume = sfxVolume;
+    }
+    public void UpdateMusicVolume()
+    {
+        musicVolume = music_volume.value;
+        musicValue.text = ((int)musicVolume).ToString();
+        gameInstance.musicVolume = musicVolume;
     }
 }
